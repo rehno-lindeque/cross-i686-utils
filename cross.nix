56916d819a8dd94acfcea3a82cf5c44c63484652
Rehno Lindeque <errantkid@gmail.com>
2016-10-28 17:14:40 +0000

    Fails at libiconv
    make  all-am
    make[2]: Entering directory '/tmp/nix-build-libiconv-1.14.drv-1/libiconv-1.14/srclib'
    make[3]: Entering directory '/tmp/nix-build-libiconv-1.14.drv-1/libiconv-1.14'
    make[3]: Nothing to be done for 'am--refresh'.
    make[3]: Leaving directory '/tmp/nix-build-libiconv-1.14.drv-1/libiconv-1.14'
    gcc -DHAVE_CONFIG_H -DEXEEXT=\"\" -I. -I.. -I../lib  -I../intl -DDEPENDS_ON_LIBICONV=1 -DDEPENDS_ON_LIBINTL=1   -g -O2 -c allocator.c
    gcc -DHAVE_CONFIG_H -DEXEEXT=\"\" -I. -I.. -I../lib  -I../intl -DDEPENDS_ON_LIBICONV=1 -DDEPENDS_ON_LIBINTL=1   -g -O2 -c areadlink.c
    gcc -DHAVE_CONFIG_H -DEXEEXT=\"\" -I. -I.. -I../lib  -I../intl -DDEPENDS_ON_LIBICONV=1 -DDEPENDS_ON_LIBINTL=1   -g -O2 -c careadlinkat.c
     44 85.8M   44 38.0M    0     0   292k      0  0:05:00  0:02:13  0:02:47   97kgcc -DHAVE_CONFIG_H -DEXEEXT=\"\" -I. -I.. -I../lib  -I../intl -DDEPENDS_ON_LIBICONV=1 -DDEPENDS_ON_LIBINTL=1   -g -O2 -c malloca.c
    gcc -DHAVE_CONFIG_H -DEXEEXT=\"\" -I. -I.. -I../lib  -I../intl -DDEPENDS_ON_LIBICONV=1 -DDEPENDS_ON_LIBINTL=1   -g -O2 -c progname.c
    In file included from progname.c:26:0:
    ./stdio.h:1010:1: error: 'gets' undeclared here (not in a function)
     _GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
     ^
    make[2]: *** [Makefile:914: progname.o] Error 1
    make[2]: Leaving directory '/tmp/nix-build-libiconv-1.14.drv-1/libiconv-1.14/srclib'
    make[1]: *** [Makefile:865: all] Error 2
    make[1]: Leaving directory '/tmp/nix-build-libiconv-1.14.drv-1/libiconv-1.14/srclib'
    make: *** [Makefile:35: all] Error 2
    builder for ‘/nix/store/w4xj53ppnx7067av70mvj4icl18y7ldc-libiconv-1.14.drv’ failed with exit code 2
    cannot build derivation ‘/nix/store/krqpd3k1a4vy54lxkr5ycdvvcvnkr1d3-gnugrep-2.25.drv’: 1 dependencies couldn't be built
    killing process 2072
    building path(s) ‘/nix/store/4szhsl0g2as36x1bv40myqi48imdbmli-readline-6.3p08-i686-unknown-linux-dev’, ‘/nix/store/jsf1cnaq1v1zvipisq1qg34phlg44gk0-readline-6.3p08-i686-unknown-linux-doc’, ‘/nix/store/r54mi03gcvjv1wq9w3li8mikj1ng349d-readline-6.3p08-i686-unknown-linux’
    cannot build derivation ‘/nix/store/88lxhw3807lflnjfckrgyv3lhzvnjjsz-gcc-wrapper-4.9.4.drv’: 1 dependencies couldn't be built
    cannot build derivation ‘/nix/store/yzamfsfzwjwkgj5rr2b6b86xpb9h167p-socat-1.7.3.1-i686-unknown-linux.drv’: 1 dependencies couldn't be built
    error: build of ‘/nix/store/yzamfsfzwjwkgj5rr2b6b86xpb9h167p-socat-1.7.3.1-i686-unknown-linux.drv’ failed

---
+++ cross.nix
@@ -0,0 +1,101 @@
+# run as
+# nix-build -A socat.crossDrv
+let
+  # Base packages
+  fetchFromGitHub = (import <nixpkgs> {}).fetchFromGitHub;
+  nixpkgs =
+    fetchFromGitHub
+      {
+        owner = "NixOS";
+        repo = "nixpkgs-channels";
+        # Nix 14.04-small
+        /* rev = "8a3eea054838b55aca962c3fbde9c83c102b8bf2"; */
+        /* sha256 = "1i2s1n7kq16932xf0p4ffgp7rs1j71yzny8kk2r2nyj48i8njqkx"; */
+        # Nix 15.09-small
+        /* rev = "a888bbacb1d37c800b183fad1e721da31206864b"; */
+        /* sha256 = "0yq9frfvnf4mscsm9w751kssclwh6mv0sq4shki0l29gshglbvig"; */
+        # Nix 16.03-small
+        /* rev = "c1c0484041ab6f9c6858c8ade80a8477c9ae4442"; */
+        /* sha256 = "14zivn0wcqh07dw2vy9n6k7s3b2xdq1x37ziqmk2j7zxgx61f5xs"; */
+        # HEAD
+        rev = "210b3b3184b27be8597f320fc9f337d3997dce94";
+        sha256 = "03gl40738zyrsg5md5l5gyi5d1f5h4h4kbliissbxi4q7qwr38v5";
+        # https://github.com/NixOS/nixpkgs/pull/18386
+        /* owner = "DavidEGrayson" */
+        /* repo = "nixpkgs"; */
+        /* rev = "873da5aa4014fb836a19f3afc2a848443ff2ede8"; */
+        /* sha256 = "0000000000000000000000000000000000000000000000000000"; */
+      };
+  /* nixpkgs = <nixpkgs>; */
+
+  # From nixpkgs/pkgs/top-level/platforms.nix
+  pcBase =
+    {
+      name = "pc";
+      uboot = null;
+      kernelHeadersBaseConfig = "defconfig";
+      kernelBaseConfig = "defconfig";
+      kernelAutoModules = true;
+      kernelTarget = "bzImage";
+    };
+
+  pc32 = pcBase // { kernelArch = "i386"; };
+
+  pc32_simplekernel = pc32 //
+    {
+      kernelAutoModules = false;
+    };
+
+in
+  import nixpkgs
+    {
+      crossSystem = {
+        /* config = "mips-unknown-linux"; */
+        config = "i686-unknown-linux";
+        bigEndian = true;
+        /* arch = "mips"; */
+        /* arch = "i686"; */
+        arch = "i386";
+        float = "soft";
+        withTLS = true;
+        /* libc = "glibc"; # "uclibc"; ? */
+        libc = "uclibc";
+        platform = pc32 // {
+          name = "tpsys";
+          kernelMajor = "2.6"; # 2.6.16.20
+          /* kernelArch = "mips32"; */
+        };
+        openssl.system = "linux-generic32";
+        gcc = {
+          arch = "i686";
+          /* arch = "mips32"; */
+        };
+      };
+      config = {
+        packageOverrides = super: let self = super.pkgs; in {
+          nix = self.stdenv.lib.overrideDerivation super.nix
+            (oldAttrs:
+              {
+                /* postUnpack="" */
+              }
+            );
+          socat = self.stdenv.lib.overrideDerivation super.socat
+            (oldAttrs:
+              {
+                gcc = self.gcc49;
+                /* libc = self.pkgs.glibc_multi; */
+                /* libc = self.pkgs.libcCross; */
+                libc = self.pkgs.uclibc;
+              }
+            );
+          /* petool = self.callPackage (self.fetchFromGitHub { */
+          /*   owner = "cnc-patch"; */
+          /*   repo = "petool"; */
+          /*   rev = "f0231058829dcb34f04d0e427b464371a44f8522"; */
+          /*   sha256 = "0qjf4bzj52j6sw4rl7nndkz335k1vjgfd13lrqwihsjhicbyj71m"; */
+          /* }) {}; */
+          /* mkCncGame = self.callPackage ./template.nix {}; */
+        };
+      };
+    }
+
